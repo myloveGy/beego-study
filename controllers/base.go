eaf1be79677b499b67d746ff5d4dd7b41a90a6ff
刘金星 <821901008@qq.com>
2021-03-15 23:14:00 +0800

    feat: 基本全部完结

--- controllers/base.go
+++ controllers/base.go
@@ -59,4 +59,5 @@ func (c *Controller) Prepare() {
 	c.Data["commList"] = commList
 	c.Data["imgList"] = imgList
 	c.Data["user"] = c.User.Username
+	c.Data["me"], _ = beego.AppConfig.GetSection("me")
 }
